b1e447ea5f593e09c2b699a38f30c8244daf8306
Mahmoud Hesham <7odahesham4478@gmail.com>
2026-01-12 14:17:57 +0200

    Creating the Repo

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const T2semaApp());
+}
+
+class T2semaApp extends StatelessWidget {
+  const T2semaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
