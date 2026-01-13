ed1fa8b05da73277549ba178ba974d3ba2c70e25
Mahmoud Hesham <7odahesham4478@gmail.com>
2026-01-13 17:49:46 +0200

    feat: add nav bar to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:t2sema/features/history/presentation/views/history_view.dart';
 import 'package:t2sema/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:t2sema/features/home/presentation/views/widgets/custom_bottom_nav_bar.dart';
 import 'package:t2sema/features/match/presentation/views/player_selection_view.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+  final List<Widget> _pages = [
+    const PlayerSelectionView(),
+    const HistoryView(),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(appBar: CustomAppBar(), body: PlayerSelectionView());
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Stack(
+        children: [
+          _pages[currentIndex],
+          Align(
+            alignment: Alignment.bottomCenter,
+
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: CustomBottomNavBar(
+                currentIndex: currentIndex,
+                onTap: (index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
