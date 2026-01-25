cb2c2b5c37d1038a9cc91f55126063fca9f225d5
Mahmoud Hesham <7odahesham4478@gmail.com>
2026-01-25 19:15:28 +0200

    refactor: removed redundant bloc provider

--- lib/features/history/presentation/views/history_view.dart
+++ lib/features/history/presentation/views/history_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:taqsema/core/utils/app_styles.dart';
-import 'package:taqsema/core/utils/service_locator.dart';
 import 'package:taqsema/core/widgets/custom_empty_state.dart';
 import 'package:taqsema/features/history/presentation/manager/history_cubit/history_cubit.dart';
 import 'package:taqsema/features/history/presentation/views/widgets/match_history_list_view.dart';
@@ -11,46 +10,43 @@ class HistoryView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<HistoryCubit>()..getAllMatches(),
-      child: Scaffold(
-        body: BlocBuilder<HistoryCubit, HistoryState>(
-          builder: (context, state) {
-            if (state is HistorySuccess) {
-              return Column(
-                children: [
-                  const SizedBox(height: 30),
-                  Text('Matches History', style: AppStyles.textStyleMedium24),
-                  const SizedBox(height: 20),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        right: 50,
-                        left: 50,
-                        bottom: 100,
-                      ),
-                      child: MatchHistoryListView(matches: state.matches),
+    return Scaffold(
+      body: BlocBuilder<HistoryCubit, HistoryState>(
+        builder: (context, state) {
+          if (state is HistorySuccess) {
+            return Column(
+              children: [
+                const SizedBox(height: 30),
+                Text('Matches History', style: AppStyles.textStyleMedium24),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      right: 50,
+                      left: 50,
+                      bottom: 100,
                     ),
+                    child: MatchHistoryListView(matches: state.matches),
                   ),
-                ],
-              );
-            } else if (state is HistoryEmpty) {
-              return const Padding(
-                padding: EdgeInsets.only(right: 20, left: 20, bottom: 80),
-                child: CustomEmptyState(
-                  title: "No Matches Yet",
-                  subtitle:
-                      "Start a game, track the score, and save your history here!",
-                  icon: Icons.sports_soccer,
                 ),
-              );
-            } else if (state is HistoryFailure) {
-              return Center(child: Text(state.errMsg));
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
+              ],
+            );
+          } else if (state is HistoryEmpty) {
+            return const Padding(
+              padding: EdgeInsets.only(right: 20, left: 20, bottom: 80),
+              child: CustomEmptyState(
+                title: "No Matches Yet",
+                subtitle:
+                    "Start a game, track the score, and save your history here!",
+                icon: Icons.sports_soccer,
+              ),
+            );
+          } else if (state is HistoryFailure) {
+            return Center(child: Text(state.errMsg));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
